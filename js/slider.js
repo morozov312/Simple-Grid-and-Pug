8db0b0ec25659f4d05aff1fe908642a73f216466
Morozov312 <alexander.morozov312@gmail.com>
2020-08-28 23:18:21 +0700

    fixed constantly decreasing of slider

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,14 @@
-if (window.matchMedia('(max-width: 768px)').matches) {
-    $('.tiles__wrap').addClass('slider');
-}
-if (window.matchMedia('(min-width: 768px)').matches) {
-    $('.tiles__wrap').removeClass('slider');
-}
-$('.slider').slick({
-    autoplay: true,
-    dots: true,
-    arrows: false
+$(document).ready(function () {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+        $('.tiles__wrap').addClass('slider');
+    }
+    if (window.matchMedia('(min-width: 768px)').matches) {
+        $('.tiles__wrap').removeClass('slider');
+    }
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false
+    });
 });
