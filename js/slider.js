234bedebd18d56558b0fb0a97fa4e479120798b1
Morozov312 <alexander.morozov312@gmail.com>
2020-08-28 22:15:38 +0700

    changed slider link logic

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,11 @@
 if (window.matchMedia('(max-width: 768px)').matches) {
-    $('.slider').slick({
-        autoplay: true,
-        dots: true,
-        arrows: false
-    });
+    $('.tiles__wrap').addClass('slider');
 }
+if (window.matchMedia('(min-width: 768px)').matches) {
+    $('.tiles__wrap').removeClass('slider');
+}
+$('.slider').slick({
+    autoplay: true,
+    dots: true,
+    arrows: false
+});
