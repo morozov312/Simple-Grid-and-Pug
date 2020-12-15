626c5a5e5415d7b50a237cd20c893f4a3d9b53d4
Morozov312 <alexander.morozov312@gmail.com>
2020-12-15 23:45:48 +0700

    update js

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,39 @@
-$(document).ready(function () {
-    if (window.matchMedia('(max-width: 768px)').matches) {
-        $('.tiles__wrap').addClass('slider');
-    }
-    if (window.matchMedia('(min-width: 768px)').matches) {
-        $('.tiles__wrap').removeClass('slider');
-    }
-    $('.slider').slick({
+function addSlider() {
+    jQuery('.slider').slick({
         autoplay: true,
         autoplaySpeed: 4000,
         dots: true,
         arrows: false
-    });
-});
+    })
+}
+jQuery(document).ready(function () {
+    let initialWidth = jQuery(window).width()
+    let needReload = false
+    let isExist = false
+    let initialTiles = jQuery('.tiles__wrap').html()
+    if (initialWidth < 768) {
+        jQuery('.tiles__wrap').addClass('slider')
+        addSlider()
+        isExist = true
+        needReload = true
+    }
+    jQuery(window).resize(function () {
+        let width = jQuery(window).width()
+        if (width < 768) {
+            jQuery('.tiles__wrap').addClass('slider')
+            if (!isExist) {
+                addSlider()
+                isExist = true
+                needReload = true
+            }
+        } else if (width > 768 && needReload) {
+            let removedWrap = jQuery('.tiles__wrap')
+            removedWrap.removeClass()
+            removedWrap.addClass('tiles__wrap content')
+            jQuery('.tiles__wrap').empty()
+            jQuery('.tiles__wrap').prepend(initialTiles)
+            needReload = false
+            isExist = false
+        }
+    })
+})
